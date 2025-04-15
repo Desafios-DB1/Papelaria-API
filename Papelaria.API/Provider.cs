800b8b2f35f42a5a2837867625f0370d9c1064df
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-15 15:10:45 -0300

    feat: cria projeto inicial da api

---
+++ Papelaria.API/Provider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Papelaria.API.Setups;
+
+namespace Papelaria.API;
+
+public static class Provider
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddValidatorsSetup();
+        
+        services.AddSwaggerGen(SwaggerSetup.ConfigureSwagger);
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "http://localhost:5127";
+                options.Audience = "papelariaapi";
+                options.RequireHttpsMetadata = false;
+            });
+
+        services.AddAuthorization();
+    }
+}
