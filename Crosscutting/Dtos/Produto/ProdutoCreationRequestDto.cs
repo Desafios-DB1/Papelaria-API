67bbfdd1def09621009ba937c8dce77183ab71fc
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-17 09:21:57 -0300

    Desabilita o nullable de API, crosscutting e domain

--- Crosscutting/Dtos/Produto/ProdutoCreationRequestDto.cs
+++ Crosscutting/Dtos/Produto/ProdutoCreationRequestDto.cs
@@ -4,7 +4,7 @@ namespace Crosscutting.Dtos.Produto;
 public class ProdutoCreationRequestDto
 {
     public string Nome { get; set; } = string.Empty;
-    public string? Descricao { get; set; }
+    public string Descricao { get; set; }
     public int QuantidadeMinima { get; set; }
     public int QuantidadeAtual { get; set; }
     public decimal PrecoCompra { get; set; }
--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -5,7 +5,7 @@ namespace Domain.Entities;
 public class Produto : Entidade<Produto>
 {
     public string Nome { get; set; }
-    public string? Descricao { get; set; }
+    public string Descricao { get; set; }
     public QuantidadeEstoque QuantidadeEstoque {get; private set; }
     public decimal PrecoCompra { get; set; }
     public decimal PrecoVenda { get; set; }
