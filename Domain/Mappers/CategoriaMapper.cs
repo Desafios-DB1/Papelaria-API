8d972e47bff25d0151f1bb1227185eec0fa091f3
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-23 10:04:37 -0300

    Cria mapper para creation dto de categoria

---
+++ Domain/Mappers/CategoriaMapper.cs
@@ -0,0 +1,16 @@
+﻿using Crosscutting.Dtos.Categoria;
+using Domain.Entities;
+
+namespace Domain.Mappers;
+
+public static class CategoriaMapper
+{
+    public static CategoriaCreationRequestDto MapToCreationDto(Categoria categoria)
+    {
+        return new CategoriaCreationRequestDto
+        {
+            Nome = categoria.Nome,
+            Descricao = categoria.Descricao
+        };
+    }
+}
