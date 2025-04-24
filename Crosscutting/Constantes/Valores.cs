a609d82ee0f66875dc5b9193d451acabc304079a
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-24 13:41:35 -0300

    feat: adiciona valor cinquenta e cem

--- Crosscutting/Constantes/Valores.cs
+++ Crosscutting/Constantes/Valores.cs
@@ -2,6 +2,8 @@ namespace Crosscutting.Constantes;
 
 public static class Valores
 {
+    public static int Cinquenta = 50;
+    public static int Cem = 100;
     public static int Duzentos = 200;
     public static int Trezentos = 300;
 }
