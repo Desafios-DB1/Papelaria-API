f1d29145a4a696abb08c35e7234b9dd889998789
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-24 13:47:01 -0300

    feat: adiciona validações para dto de login

---
+++ Crosscutting/Validators/Auth/Login/LoginRequestDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Crosscutting.Constantes;
+using Crosscutting.Dtos.Auth.Login;
+using FluentValidation;
+
+namespace Crosscutting.Validators.Auth.Login;
+
+public class LoginRequestDtoValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage(ValidationErrors.CampoObrigatorio)
+            .MaximumLength(Valores.Duzentos).WithMessage(ValidationErrors.TamanhoMaximo);
+        RuleFor(x => x.Senha)
+            .NotEmpty().WithMessage(ValidationErrors.CampoObrigatorio)
+            .MaximumLength(Valores.Cinquenta).WithMessage(ValidationErrors.TamanhoMaximo);
+    }
+}
