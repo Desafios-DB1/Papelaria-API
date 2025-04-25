0e474e7497910b4cbf5c90c11847eb23f68a8d3f
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-25 13:52:37 -0300

    feat: adiciona entidade log produto

---
+++ Domain/Entities/LogProduto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class LogProduto
+{
+    public Guid Id { get; set; }
+    
+    public Guid ProdutoId { get; set; }
+    public Produto Produto { get; set; }
+    
+    public Guid UsuarioId { get; set; }
+    public Usuario Usuario { get; set; }
+    
+    public TipoOperacao TipoOperacao { get; set; }
+    public int QuantidadeAnterior { get; set; }
+    public int QuantidadeAtual { get; set; }
+    
+    public DateTime DataAlteracao { get; set; } = DateTime.Now;
+}
