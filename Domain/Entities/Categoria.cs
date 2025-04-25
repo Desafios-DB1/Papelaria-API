62a187d67a72218b31eb14adff866d2a74b81db2
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-25 14:01:32 -0300

    feat: adiciona coleção de produtos na entidade categoria

--- Domain/Entities/Categoria.cs
+++ Domain/Entities/Categoria.cs
@@ -6,5 +6,7 @@ public class Categoria : Entidade<Categoria>
     public string Descricao { get; set; }
     public bool Ativo { get; set; } = true;
     
+    public ICollection<Produto> Produtos { get; set; } = [];
+    
     public Categoria() {}
 }
