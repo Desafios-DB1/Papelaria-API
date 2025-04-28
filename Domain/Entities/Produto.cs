0dad6e53e5bd4736aad300603eb98e5eaca434f5
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-28 08:49:37 -0300

    refactor: remove propriedade não usada em Entidade Genérica

--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -2,7 +2,7 @@ using Domain.ValueObjects;
 
 namespace Domain.Entities;
 
-public class Produto : Entidade<Produto>
+public class Produto : Entidade
 {
     public string Nome { get; set; }
     public string Descricao { get; set; }
