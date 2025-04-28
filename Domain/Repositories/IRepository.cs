97747fe6825fbcb2eebde13963b0bd668c8b3ddd
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-28 09:25:50 -0300

    feat: adiciona interface para métodos genéricos

---
+++ Domain/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    public Task SalvarAsync();
+    public Task<Guid> AdicionarAsync(T entity);
+    public Task<Guid> AdicionarESalvarAsync(T entity);
+    public Task<T> ObterPorIdAsync(Guid id);
+    public Task<List<T>> ObterTodosAsync();
+    public Guid Atualizar(T entity);
+    public Task<Guid> AtualizarESalvarAsync(T entity);
+    public void Remover(T entity);
+    public Task RemoverESalvarAsync(T entity);
+}
