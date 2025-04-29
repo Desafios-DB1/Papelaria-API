c8bf5724b79af8aa0cd0c34a9da4a537061d430b
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-29 10:53:48 -0300

    refactor: ajusta entidade produto no banco

---
+++ Infra/Migrations/20250429135125_AjustaEntidadeProduto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustaEntidadeProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstoqueCritico",
+                table: "Produtos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EstoqueCritico",
+                table: "Produtos",
+                type: "bit",
+                nullable: true);
+        }
+    }
+}
