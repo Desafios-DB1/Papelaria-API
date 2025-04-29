102dc74fe5fa7c0e1da0a55425a1124eee8d6f58
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-04-29 15:05:20 -0300

    feat: adiciona categoriaId para criação de produto

--- Crosscutting/Dtos/Produto/ProdutoCreationRequestDto.cs
+++ Crosscutting/Dtos/Produto/ProdutoCreationRequestDto.cs
@@ -5,6 +5,7 @@ public class ProdutoCreationRequestDto
 {
     public string Nome { get; set; }
     public string Descricao { get; set; }
+    public Guid CategoriaId { get; set; }
     public int QuantidadeMinima { get; set; }
     public int QuantidadeAtual { get; set; }
     public decimal PrecoCompra { get; set; }
