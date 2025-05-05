fb0cc6b79a59b1d1db6ce853b77caba94bce3d03
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-05 10:11:02 -0300

    feat: adiciona repositorio de log produto

---
+++ Infra/Repositories/LogProdutoRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories;
+
+public class LogProdutoRepository(ApplicationDbContext context) : Repository<LogProduto>(context), ILogProdutoRepository
+{
+    public async Task<List<LogProduto>> ObterPorProdutoIdAsync(Guid produtoId)
+    {
+        var logs = await Context.LogsProduto
+            .AsNoTracking()
+            .Where(l => l.ProdutoId == produtoId)
+            .ToListAsync();
+
+        return logs;
+    }
+
+    public async Task<List<LogProduto>> ObterPorUsuarioIdAsync(Guid usuarioId)
+    {
+        var logs = await Context.LogsProduto
+            .AsNoTracking()
+            .Where(l => l.UsuarioId == usuarioId)
+            .ToListAsync();
+
+        return logs;
+    }
+}
