d27b02e4cac1b0f392d200664678889ecb4630bc
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-06 14:59:50 -0300

    feat: adicona novos erros de validação

--- Crosscutting/Constantes/ValidationErrors.cs
+++ Crosscutting/Constantes/ValidationErrors.cs
@@ -3,10 +3,11 @@ namespace Crosscutting.Constantes;
 public static class ValidationErrors
 {
     public static string CampoObrigatorio => "{PropertyName} é obrigatório.";
-    public static string CampoInvalido => "{PropertyName} é inválido.";
+    public static string CampoInvalido(string campo) => $"Campo inválido: {campo}";
     public static string TamanhoMaximo => "{PropertyName} deve ter no máximo {MaxLength} caracteres.";
     public static string ValorMinimo => "{PropertyName} deve ser no mínimo {ComparisonValue}.";
     public static string ValorNaoNegativo => "{0} não pode ser menor que zero!";
     public static string QuantidadeInsuficiente => "Quantidade insuficiente em estoque!";
     public static string EmailInvalido => "'{PropertyValue}' não é um email válido.";
+    public static string ErrosDeValidacao => "A requisição possui erros de validação";
 }
