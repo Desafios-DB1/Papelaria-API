49b59b840b8f27432e830a6a73f43ee14c96a30a
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-06 15:01:58 -0300

    refactor: adiciona validações no CriarAsync

--- Domain/Services/CategoriaService.cs
+++ Domain/Services/CategoriaService.cs
@@ -1,15 +1,42 @@
-﻿using Crosscutting.Dtos.Categoria;
+﻿using Crosscutting.Constantes;
+using Crosscutting.Dtos.Categoria;
+using Crosscutting.Exceptions;
 using Domain.Interfaces;
 using Domain.Mappers;
 using Domain.Repositories;
+using FluentValidation;
 
 namespace Domain.Services;
 
-public class CategoriaService(ICategoriaRepository repository) : ICategoriaService
+public class CategoriaService(ICategoriaRepository repository, 
+    IValidator<CategoriaCreationRequestDto> creationValidator) : ICategoriaService
 {
-    public Task<Guid> CriarAsync(CategoriaCreationRequestDto categoriaDto)
+    public async Task<Guid> CriarAsync(CategoriaCreationRequestDto categoriaDto)
     {
-        var categoria = categoriaDto.MapToEntity();
-        return repository.AdicionarESalvarAsync(categoria);
+        if (categoriaDto is null)
+            throw new RequisicaoInvalidaException(ValidationErrors.CampoInvalido("categoria"));
+
+        var validationResult = await creationValidator.ValidateAsync(categoriaDto);
+        if (!validationResult.IsValid)
+            throw new RequisicaoInvalidaException(validationResult.Errors);
+
+        try
+        {
+            var categoria = categoriaDto.MapToEntity();
+            var newCategoriaId = await repository.AdicionarESalvarAsync(categoria);
+
+            if (newCategoriaId == Guid.Empty)
+                throw new ErroNoBancoException(ErrorMessages.BancoFalhou);
+
+            return newCategoriaId;
+        }
+        catch (ErroNoBancoException ex)
+        {
+            throw new ErroNoBancoException(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            throw new ErroDesconhecidoException(ErrorMessages.ErroDesconhecido, ex);
+        }
     }
 }
