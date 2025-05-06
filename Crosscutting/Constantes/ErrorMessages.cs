7a29bbaf5d28b625059f00f8b5ac28012ce08a17
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-06 15:00:05 -0300

    feat: adiciona novas mensagens de erro

---
+++ Crosscutting/Constantes/ErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Crosscutting.Constantes;
+
+public static class ErrorMessages
+{
+    public static string BancoFalhou => "Ocorreu um erro no banco de dados.";
+    public static string ErroDesconhecido => "Ocorreu um erro desconhecido.";
+}
