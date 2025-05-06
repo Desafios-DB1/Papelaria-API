3430248592c75f67b5c79894e36258d79fdbde12
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-06 09:01:50 -0300

    feat: cria serviço e método para criar categoria

---
+++ Domain/Services/CategoriaService.cs
@@ -0,0 +1,15 @@
+﻿using Crosscutting.Dtos.Categoria;
+using Domain.Interfaces;
+using Domain.Mappers;
+using Domain.Repositories;
+
+namespace Domain.Services;
+
+public class CategoriaService(ICategoriaRepository repository) : ICategoriaService
+{
+    public Task<Guid> CriarAsync(CategoriaCreationRequestDto categoriaDto)
+    {
+        var categoria = categoriaDto.MapToEntity();
+        return repository.AdicionarESalvarAsync(categoria);
+    }
+}
