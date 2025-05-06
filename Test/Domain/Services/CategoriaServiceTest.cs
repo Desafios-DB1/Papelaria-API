6d12f5e8ccd864351186f217b0ca61768b22686b
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-06 09:14:06 -0300

    feat: testes para categoria service

---
+++ Test/Domain/Services/CategoriaServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Mappers;
+using Domain.Repositories;
+using Domain.Services;
+using FluentAssertions;
+using Moq;
+using Test.Domain.Builders;
+
+namespace Test.Domain.Services;
+
+public class CategoriaServiceTest
+{
+    private readonly CategoriaService _categoriaService;
+    private readonly Mock<ICategoriaRepository> _categoriaRepositoryMock = new();
+
+    public CategoriaServiceTest()
+    {
+        _categoriaService = new CategoriaService(_categoriaRepositoryMock.Object);
+    }
+    
+    #region CriarCategoria
+    
+    [Fact]
+    public async Task CriarAsync_QuandoDtoValido_DeveRetornarId()
+    {
+        var categoria = CategoriaBuilder.Novo().Build();
+        
+        _categoriaRepositoryMock.Setup(r => r.AdicionarESalvarAsync(It.IsAny<Categoria>()))
+            .ReturnsAsync(categoria.Id);
+
+        var result = await _categoriaService.CriarAsync(categoria.MapToCreationDto());
+        result.Should().Be(categoria.Id);
+    }
+    #endregion
+}
