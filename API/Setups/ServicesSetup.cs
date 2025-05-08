afe12390f9c4696467e16774536b18e5f4d4c070
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-08 10:03:46 -0300

    feat: adiciona configuração de serviços ao setup

---
+++ API/Setups/ServicesSetup.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces;
+using Domain.Services;
+
+namespace API.Setups;
+
+public static class ServicesSetup
+{
+    public static IServiceCollection AddServicesSetup(this IServiceCollection services)
+    {
+        services
+            .AddScoped<ICategoriaService, CategoriaService>();
+        
+        return services;
+    }
+}
