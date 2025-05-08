8270a0054f0ca92d10a12bde8cd11e452e7e9e88
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-08 09:34:11 -0300

    refactor: renomeia parâmetro na mensagem de erro para maior clareza

--- Crosscutting/Constantes/ErrorMessages.cs
+++ Crosscutting/Constantes/ErrorMessages.cs
@@ -7,5 +7,5 @@ public static class ErrorMessages
             ? $"O campo {campo} não pode ser nulo."
             : $"O campo {campo} do objeto {objeto} não pode ser nulo.";
     public static string ObjetoNulo(string objeto) => $"O objeto {objeto} não pode ser nulo.";
-    public static string NaoExiste(string campo) => $"{campo} não existe.";
+    public static string NaoExiste(string item) => $"{item} não existe.";
 }
