905dc6ac649b9a213547b462e6fa1c5008447b12
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-13 14:42:24 -0300

    feat: cria testes de controller para CriarProduto

---
+++ Test/API/ProdutoControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using API.Controllers;
+using Crosscutting.Dtos.Produto;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Test.API;
+
+public class ProdutoControllerTest
+{
+    private readonly Mock<IMediator> _mediator;
+    private readonly ProdutoController _controller;
+    
+    public ProdutoControllerTest()
+    {
+        _mediator = new Mock<IMediator>();
+        _controller = new ProdutoController(_mediator.Object);
+    }
+    
+    [Fact]
+    public async Task CriarProduto_QuandoProdutoCriadoComSucesso_DeveRetornarCreatedAtAction()
+    {
+        var produtoDto = new ProdutoDto();
+        var produtoId = Guid.NewGuid();
+
+        _mediator
+            .Setup(m => m.Send(It.IsAny<IRequest<Guid>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(produtoId);
+        
+        var result = await _controller.CriarProduto(produtoDto, CancellationToken.None);
+        
+        var createdResult = result.Should().BeOfType<CreatedAtActionResult>().Subject;
+        createdResult.Value.Should().Be(produtoId);
+    }
+
+    [Fact]
+    public async Task CriarProduto_DeveRetornarBadRequest_QuandoProdutoNaoForCriado()
+    {
+        var produtoDto = new ProdutoDto();
+
+        _mediator
+            .Setup(m => m.Send(It.IsAny<IRequest<Guid>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Guid.Empty);
+        
+        var result = await _controller.CriarProduto(produtoDto, CancellationToken.None);
+
+        result.Should().BeOfType<BadRequestResult>();
+    }
+}
