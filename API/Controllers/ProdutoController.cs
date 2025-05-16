2a4d1ce84c8e2516910f815d36bd59d97129688a
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-16 15:55:11 -0300

    feat: implementa comando para alterar estoque de produtos e adiciona endpoint no controller

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -103,4 +103,16 @@ public class ProdutoController(IMediator mediator, IProdutoQuery query) : Contro
         var result = await query.ObterPorStatusEstoque(statusEstoque);
         return Ok(result);
     }
+    
+    [Authorize]
+    [HttpPatch("estoque")]
+    [ProducesResponseType(typeof(ProdutoDto), 200)]
+    [ProducesResponseType(typeof(ErrorResponse),404)]
+    [ProducesResponseType(typeof(ErrorResponse),401)]
+    public async Task<IActionResult> AlterarEstoque([FromBody] AlterarEstoqueCommand request, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(request, cancellationToken);
+        
+        return result is null ? NotFound() : Ok(result);
+    }
 }
