439bb8083bed153d6a05056b12c3f2e2d18833fc
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-16 14:22:24 -0300

    feat: adiciona método para remover produto por id

--- Domain/Commands/Produto/RemoverProdutoCommand.cs
+++ Domain/Commands/Produto/RemoverProdutoCommand.cs
@@ -1,6 +1,25 @@
-﻿namespace Domain.Commands.Produto;
+﻿using Crosscutting.Constantes;
+using Crosscutting.Exceptions;
+using Domain.Repositories;
+using MediatR;
 
-public class RemoverProdutoCommand
+namespace Domain.Commands.Produto;
+
+public class RemoverProdutoCommand : IRequest
 {
-    
+    public Guid id { get; set; }
+}
+
+public class RemoverProdutoCommandHandler(IProdutoRepository repository) : IRequestHandler<RemoverProdutoCommand>
+{
+    public async Task Handle(RemoverProdutoCommand request, CancellationToken cancellationToken)
+    {
+        if (request is null)
+            throw new RequisicaoInvalidaException(ErrorMessages.RequisicaoInvalida);
+        
+        var entity = await repository.ObterPorIdAsync(request.id)
+            ?? throw new NaoEncontradoException(ErrorMessages.NaoExiste("Produto"));
+        
+        await repository.RemoverESalvarAsync(entity);
+    }
 }
