f73ea4c407890fea5b6e1eda62df500d0efd81d9
Lucas Gonçalves <lucasemanuel.gon@gmail.com>
2025-05-18 12:19:41 -0300

    # This is the 1st commit message:
    feat: adiciona método e endpoint para criar categoria
    
    # This is the commit message #2:
    
    fix: ajusta método de criar categoria para POST

---
+++ Domain/Commands/Categoria/CriarCategoriaCommand.cs
@@ -0,0 +1,27 @@
+﻿using Crosscutting.Constantes;
+using Crosscutting.Exceptions;
+using Domain.Mappers;
+using Domain.Repositories;
+using MediatR;
+
+namespace Domain.Commands.Categoria;
+
+public class CriarCategoriaCommand : IRequest<Guid>
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public bool Ativo { get; set; }
+}
+
+public class CriarCategoriaCommandHandler(ICategoriaRepository repository) : IRequestHandler<CriarCategoriaCommand, Guid>
+{
+    public async Task<Guid> Handle(CriarCategoriaCommand request, CancellationToken cancellationToken)
+    {
+        if (request is null)
+            throw new RequisicaoInvalidaException(ErrorMessages.RequisicaoInvalida);
+
+        var categoria = request.MapToEntity();
+
+        return await repository.AdicionarESalvarAsync(categoria);
+    }
+}
