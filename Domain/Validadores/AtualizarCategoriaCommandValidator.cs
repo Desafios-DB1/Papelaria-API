17dbb2c1e0535654eb49c135b9b91376f810d1fb
lucas.goncalves <lucas.goncalves@db1.com.br>
2025-05-19 09:46:08 -0300

    feat: adiciona comando e validador para atualizar categoria

---
+++ Domain/Validadores/AtualizarCategoriaCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using Crosscutting.Constantes;
+using Domain.Commands.Categoria;
+using Domain.Repositories;
+using FluentValidation;
+
+namespace Domain.Validadores;
+
+public class AtualizarCategoriaCommandValidator : AbstractValidator<AtualizarCategoriaCommand>
+{
+    public AtualizarCategoriaCommandValidator(ICategoriaRepository repository)
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage(ValidationErrors.CampoObrigatorio);
+        
+        RuleFor(x => x.Nome)
+            .NotEmpty().WithMessage(ValidationErrors.CampoObrigatorio)
+            .MaximumLength(Valores.Duzentos).WithMessage(ValidationErrors.TamanhoMaximo)
+            .Must(nome => !repository.ExisteComNome(nome))
+            .WithMessage(ValidationErrors.JaExiste(Entidades.Produto));
+
+        RuleFor(x=>x.Descricao)
+            .MaximumLength(Valores.Trezentos).WithMessage(ValidationErrors.TamanhoMaximo);
+    }
+}
